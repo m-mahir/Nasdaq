266a9bfa84abf06d4e81a027b45a954e9c6d97fa
Mohamed Maher <mohammed.mahir92@gmail.com>
2022-08-06 01:08:06 +0200

    E2E Cypress Test
    Explore Screen

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,35 @@
-describe("E2E Test", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:19006/");
+import { API_KEY, BASE_URL } from "../../src/config";
+
+describe("E2E", () => {
+  it("displays stock list", () => {
+    cy.intercept(
+      `${BASE_URL}/v3/reference/tickers?active=true&sort=ticker&order=asc&limit=10&apiKey=${API_KEY}`
+    ).as("stocks");
+    cy.visit("/");
+    cy.wait("@stocks")
+      .its("response.body.results")
+      .should("be.an", "Array")
+      .and("have.length", 10)
+      .then((stocks) => {
+        cy.get("[data-testId=item]").should("have.length", stocks.length);
+      });
   });
 
-  it("displays stock list", () => {
-    cy.contains("AA").should("be.visible");
-    //   cy.get('.todo-list li').should('have.length', 2)
-
-    //   cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    //   cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+  it("shows loading indicator", () => {
+    // slow down the response by 1 second
+    // https://on.cypress.io/intercept
+    cy.intercept(
+      `${BASE_URL}/v3/reference/tickers?active=true&sort=ticker&order=asc&limit=10&apiKey=${API_KEY}`,
+      (req) => {
+        // use bundled Bluebird library
+        // which has utility method .delay
+        // https://on.cypress.io/promise
+        return Cypress.Promise.delay(1000).then(() => req.continue());
+      }
+    ).as("stocks");
+    cy.visit("/");
+    cy.get("[data-testid=loader]").should("be.visible");
+    cy.get("[data-testid=loader]").should("not.exist");
+    cy.wait("@stocks");
   });
 });
